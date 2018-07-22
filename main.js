e5f509b6cc7a41b1f2d37e00227507c4820b1723
cuilei5205189 <allen5205189@gmail.com>
2018-07-22 20:29:28 +0800

    cancas大小始终等于浏览器窗口大小

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ var sketchBook = document.getElementById('sketchBook')
 var context = sketchBook.getContext('2d');
 
 
-context.fillStyle = 'rgb(200,0,0)';
-context.fillRect(10, 10, 55, 50);
-
+//设置canvas尺寸等于浏览器窗口尺寸
+sketchBook.width = window.innerWidth;
+sketchBook.height = window.innerHeight;
+window.onresize = function (e) {
+    sketchBook.width = window.innerWidth;
+    sketchBook.height = window.innerHeight;
+}
 
 function drawLine(x1, y1, x2, y2) {
     context.beginPath()
@@ -39,7 +43,7 @@ document.onmousemove = function (e) {
         lastPoint.x = x
         lastPoint.y = y
         drawLine(firstPoint.x, firstPoint.y, lastPoint.x, lastPoint.y)
-        firstPoint = lastPoint
+        firstPoint = lastPoint //在canvas可以画出连续的线
     }
 }
 
